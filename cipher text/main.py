d8d0d9b4b090e765e8a200827e2f21fe47333e8e
mahtab hussain <103880201+mhtab786@users.noreply.github.com>
2022-08-08 14:29:35 +0530

    Add files via upload

---
+++ cipher text/main.py
@@ -0,0 +1,37 @@
+from art import logo
+
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"here's the {cipher_direction}d result: {end_text}\n\n")
+
+
+should_continue = True
+while should_continue:
+    direction = input("type 'encode' to encrypt, 'decode' to decrypt:\n")
+    text = input("type your message \n").lower()
+    shift = int(input("type the shift number \n"))
+
+    shift = shift % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    result = input("do you want to continue\n type 'yes' or 'no'\n")
+    if result == "no":
+        should_continue = False
+        print("goodbye")
